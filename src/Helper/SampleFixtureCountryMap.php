92e8c6adacd9fa75ce3bf65c1bbcbd8c1e6a946d
Vadim <vadim.borinski@bigcommerce.com>
2018-01-19 12:36:09 +1100

    add country name and update home page for convenience

--- src/Helper/SampleFixtureCountryMap.php
+++ src/Helper/SampleFixtureCountryMap.php
@@ -11,10 +11,12 @@ class SampleFixtureCountryMap
 
     private static $mapping = [
         'US' => [
+            'countryName' => 'United States',
             'description' => '2 carriers, 3 rates similar to NZ response',
             'filename' => 'sampleResponse.json',
         ],
         'CA' => [
+            'countryName' => 'Canadia',
             'description' =>
                 'Response with 3 quotes with a dispatch and a transit time, 
                 from one carrier, with no carrier info, with rate ids
@@ -22,42 +24,51 @@ class SampleFixtureCountryMap
             'filename' => 'sampleResponseWithTimeInfo.json',
         ],
         'AU' => [
+            'countryName' => 'Australia',
             'description' =>
                 'Response with 3 quotes with info and warning messages, 
                 from one carrier, with no carrier info, with no rate ids',
             'filename' => 'sampleResponseWithWarnings.json',
         ],
         'NZ' => [
+            'countryName' => 'New Zealand',
             'description' =>
                 'Response with rates for 2 carriers, one with a single rate and 
 	            one with 2 rates, both with carrier info',
             'filename' => 'sampleResponseFromMultipleCarriers.json',
         ],
         'JP' => [
+            'countryName' => 'Japan',
             'description' => 'Response with empty quote list',
             'filename' => 'sampleResponseWithEmptyQuotes.json',
         ],
         'AG' => [
+            'countryName' => 'Antigua and Barbuda',
             'description' => 'Response with missing top level "messages" key',
             'filename' => 'sampleInvalidResponse1.json',
         ],
         'AQ' => [
+            'countryName' => 'Antarctica',
             'description' => 'Response with missing top level "quote_id" key',
             'filename' => 'sampleInvalidResponse2.json',
         ],
         'AI' => [
+            'countryName' => 'Anguilla',
             'description' => 'Response with missing top level "carrier_quotes" key',
             'filename' => 'sampleInvalidResponse3.json',
         ],
         'BS' => [
+            'countryName' => 'Bahamas',
             'description' => 'Response with a rate with missing "cost" key',
             'filename' => 'sampleInvalidResponse4.json',
         ],
         'BH' => [
+            'countryName' => 'Bahrain',
             'description' => 'Response with a carrier info with missing "code" key',
             'filename' => 'sampleInvalidResponse5.json',
         ],
         'BD' => [
+            'countryName' => 'Bangladesh',
             'description' => 'Response with a rate with a message missing a "type" key',
             'filename' => 'sampleInvalidResponse6.json',
         ]
